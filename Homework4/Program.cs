c33c4ed80259a88a618215923c7028e127d81b1a
MaxTsember <maxtsember@gmail.com>
2023-04-27 19:32:57 +0300

    homework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,16 @@
+﻿//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Input Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
