2a44510c3bf835a5a75dd7a5dac6cf5debf7cfbd
MaxTsember <maxtsember@gmail.com>
2023-04-27 19:27:24 +0300

    homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
